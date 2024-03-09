73f5c6c202b27022f5804ec10226f30933f639da
harshshrivastava15 <harshshrivastava.work@gmail.com>
2024-03-09 15:32:11 +0530

    new feature

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.getElementById("menuButton").addEventListener("click", function() {
-    var menu = document.getElementById("menu");
+    var menu = document.getElementById("menublock");
     if (menu.style.display === "none" || menu.style.display === "") {
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
     }
 });
+
